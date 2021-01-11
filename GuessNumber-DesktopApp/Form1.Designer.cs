6b73f25d53f2c588b885974e43dedad558df7d39
BORIS-DESKTOP\boris <borisdimitrov1998@gmail.com>
2021-01-11 16:29:16 +0200

    Added Guess number GUI version

---
+++ GuessNumber-DesktopApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace GuessNumber_DesktopApp
+{
+    partial class GuessNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GuessNumber));
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.InfoText = new System.Windows.Forms.Label();
+            this.genText = new System.Windows.Forms.Label();
+            this.tryBtn = new System.Windows.Forms.Button();
+            this.lifeText = new System.Windows.Forms.Label();
+            this.randTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            resources.ApplyResources(this.textBox, "textBox");
+            this.textBox.Name = "textBox";
+            // 
+            // InfoText
+            // 
+            resources.ApplyResources(this.InfoText, "InfoText");
+            this.InfoText.Name = "InfoText";
+            // 
+            // genText
+            // 
+            resources.ApplyResources(this.genText, "genText");
+            this.genText.Name = "genText";
+            // 
+            // tryBtn
+            // 
+            resources.ApplyResources(this.tryBtn, "tryBtn");
+            this.tryBtn.FlatAppearance.BorderSize = 0;
+            this.tryBtn.Name = "tryBtn";
+            this.tryBtn.UseVisualStyleBackColor = true;
+            this.tryBtn.Click += new System.EventHandler(this.tryBtn_Click);
+            // 
+            // lifeText
+            // 
+            resources.ApplyResources(this.lifeText, "lifeText");
+            this.lifeText.Name = "lifeText";
+            // 
+            // randTxt
+            // 
+            resources.ApplyResources(this.randTxt, "randTxt");
+            this.randTxt.Name = "randTxt";
+            // 
+            // GuessNumber
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.randTxt);
+            this.Controls.Add(this.lifeText);
+            this.Controls.Add(this.tryBtn);
+            this.Controls.Add(this.genText);
+            this.Controls.Add(this.InfoText);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "GuessNumber";
+            this.Load += new System.EventHandler(this.GuessNumber_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label InfoText;
+        private System.Windows.Forms.Label genText;
+        private System.Windows.Forms.Button tryBtn;
+        private System.Windows.Forms.Label lifeText;
+        private System.Windows.Forms.Label randTxt;
+    }
+}
+
---
+++ GuessNumber-DesktopApp/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace GuessNumber_DesktopApp
+{
+    public partial class GuessNumber : Form
+    {
+        //Random three digit-number
+        int Min = 0;
+        int Min2 = 1;
+        int Max = 9;
+        int[] randomNumber = new int[3];
+        List<int> aux = new List<int>();
+
+        //User
+        string userNumber;
+        bool win = false;
+        int life = 15;
+        bool lose = false;
+
+
+        public GuessNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void GuessNumber_Load(object sender, EventArgs e)
+        {
+            Random randNum = new Random();
+            for (int i = 0; i < randomNumber.Length; i++)
+            {
+                if (i == 0)
+                {
+                    randomNumber[i] = randNum.Next(Min2, Max);
+                }
+                else
+                {
+                    randomNumber[i] = randNum.Next(Min, Max);
+                }
+                aux.Add(randomNumber[i]);
+                Console.Write(randomNumber[i]);
+            }
+
+            randTxt.Text = "...";
+            //Generated number and displayed on the form
+            //randTxt.Text = String.Join(",", aux).Replace(",", "");
+        }
+
+
+
+        private void tryBtn_Click(object sender, EventArgs e)
+        {
+            if (life > 0)
+            {
+                try
+                {
+                    userNumber = textBox.Text;
+
+
+                    if (Convert.ToInt32(userNumber) < 100 || Convert.ToInt32(userNumber) > 999)
+                    {
+                        InfoText.Text = "Please enter only 3 digit numbers!";
+                    }
+                    else
+                    {
+
+                        int firstNum = Convert.ToInt32(userNumber) / 100;
+                        int secondNum = Convert.ToInt32(userNumber) / 10;
+                        secondNum = secondNum % 10;
+                        int thirdNum = Convert.ToInt32(userNumber) % 10;
+
+
+                        var firstItem = aux.ElementAt(0);
+                        var secondItem = aux.ElementAt(1);
+                        var thirdItem = aux.ElementAt(2);
+
+                        bool firstGuessedNoPos = aux.IndexOf(firstNum) != -1;
+                        bool secondGuessedNoPos = aux.IndexOf(secondNum) != -1;
+                        bool thirdGuessedNoPos = aux.IndexOf(thirdNum) != -1;
+
+
+                        if (firstNum == firstItem && secondNum == secondItem && thirdNum == thirdItem)
+                        {
+                            win = true;
+                            InfoText.Text = "You guessed the number!";
+                            tryBtn.Enabled = false;
+                        }
+                        else if (firstNum == firstItem && secondNum == secondItem || firstNum == firstItem && thirdNum == thirdItem || secondNum == secondItem && thirdNum == thirdItem)
+                        {
+                            InfoText.Text = "You guessed two numbers and their position";
+                        }
+                        else if (firstNum == firstItem || secondNum == secondItem || thirdNum == thirdItem)
+                        {
+                            InfoText.Text = "You guessed one number and position";
+
+                            //Checking other two if they are included
+                            if (firstNum != firstItem || secondNum != secondItem || thirdNum != thirdItem)
+                            {
+                                InfoText.Text = "You guessed one number and position and the other two maybe are included";
+                            }
+                        }
+                        else if (firstGuessedNoPos == true && secondGuessedNoPos == true && thirdGuessedNoPos == true)
+                        {
+                            InfoText.Text = "You guessed all numbers but not the positions";
+                        }
+                        else if (firstGuessedNoPos == true && secondGuessedNoPos == true || firstGuessedNoPos == true && thirdGuessedNoPos == true || secondGuessedNoPos == true && thirdGuessedNoPos == true)
+                        {
+                            InfoText.Text = "You guessed two numbers but not the positions";
+                        }
+                        else if (firstGuessedNoPos == true || secondGuessedNoPos == true || thirdGuessedNoPos == true)
+                        {
+                            InfoText.Text = "You guessed one number but not the position";
+                        }
+                        else
+                        {
+                            InfoText.Text = "No matches";
+                        }
+
+                        life--;
+                        lifeText.Text = "Life: " + life.ToString();
+                    }
+                }
+                catch (Exception)
+                {
+                    InfoText.Text = "This was not a number!";
+                }
+            }
+            else if (life == 0)
+            {
+                lose = true;
+                InfoText.Text = "You Lose!";
+                tryBtn.Enabled = false;
+                randTxt.Text = String.Join(",", aux).Replace(",", "");
+            }
+            else if (win == true)
+            {
+                InfoText.Text = "You Win!";
+                tryBtn.Enabled = false;
+            }
+        }
+    }
+}
